514dafc17c0dbec42a2b0c4a7c6f20c55ca58177
Seth T <seth10@users.noreply.github.com>
2019-10-20 19:31:06 -0400

    Update app.js

--- app.js
+++ app.js
@@ -4,15 +4,15 @@ import { Card } from "./card";
 
 let humanPlayer = new Player();
 let cpuPlayer = new Player();
+let players = [humanPlayer, cpuPlayer];
+
 let board = new Board();
 
 for (let i = 0; i < 8; i++) {
-  humanPlayer.hand.push(new Card("Apprentice")); // TODO: create Card constructor
-  cpuPlayer.hand.push(new Card("Apprentice"));
+  players.forEach(player => player.hand.push(new Card("Apprentice")));
 }
 for (let i = 0; i < 2; i++) {
-  humanPlayer.hand.push(new Card("Militia"));
-  cpuPlayer.hand.push(new Card("Militia"));
+  players.forEach(player => player.hand.push(new Card("Militia")));
 }
 
 board.startGame();
